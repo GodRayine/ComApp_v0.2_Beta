d1979d1e639e871eaa36ea6e698d66d1feac0125
GodRayine <85738802+GodRayine@users.noreply.github.com>
2022-03-22 21:34:53 +0300

    Добавьте файлы проекта.

---
+++ AppWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ComApp_v0._2_Beta
+{
+    /// <summary>
+    /// Логика взаимодействия для AppWindow.xaml
+    /// </summary>
+    public partial class AppWindow : Window
+    {
+        public AppWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComApp_v0._2_Beta
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void CloseButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SettingsSaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            AppWindow appWindow = new AppWindow();
+            appWindow.Show();
+
+        }
+    }
+}
